902f92c5514e8341956118be0c072d3c14ebf40f
Mathias <mathias.strom03@gmail.com>
2025-03-05 11:52:23 +0100

    Added files and setup for API

--- data/raw/vaerdata.py
+++ data/raw/vaerdata.py
@@ -1,19 +1,26 @@
 import requests
+import json
 
+# Henter data fra met.no sin API
 def hent_vaerdata(lat: float, lon: float):
     url = f"https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={lat}&lon={lon}"
     headers = {
-        "User-Agent": "Miljødataanalyseapplikasjon/1.0"
+        "User-Agent": "Miljødataanalyseapplikasjon"
     }
     
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=headers, timeout=10)  # 10 sek timeout
+        response.raise_for_status()  # Viser feil hvis statuskode er 4xx/5xx
         return response.json()
-    else:
-        print(f"Feil ved henting av data: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Feil ved henting av data: {e}")
         return None
 
-# Tester for Trondheim
+# Henter data for Trondheim, og skriver data til fil hvis det finnes
 data = hent_vaerdata(63.4305, 10.3951)
-print(data)
+if data:
+    with open("data/raw/vaerdata.json", "w") as f:
+        json.dump(data, f, indent=4)
+    print("Data lagret til data/raw/vaerdata.json")
+else:
+    print("Ingen data mottatt.")
