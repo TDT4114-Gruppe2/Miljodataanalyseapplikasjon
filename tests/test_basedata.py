de6f649179accf2cd4ab83bd960b177b4ae1e297
SanderBuSu <sander.sundbye@guttasas.no>
2025-04-22 23:07:01 +0200

    Fix for basedata test

--- tests/test_basedata.py
+++ tests/test_basedata.py
@@ -2,6 +2,9 @@ import os
 import pandas as pd
 import pytest
 from datetime import datetime
+import sys
+# Legg til prosjektets rotmappe i sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.analysis.basedata import DataLoader
 
 
